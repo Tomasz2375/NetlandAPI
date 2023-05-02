a07c08ba74400b89149188018be60da44daf78fd
Tomasz <trnosal@gmail.com>
2023-05-02 12:38:27 +0200

    Add swagger

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using NetlandAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 builder.Services.AddScoped<IAppsettingsDevelopment, AppsettingsDevelopment>();
 builder.Services.AddScoped<ICsvService, CsvService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -17,13 +19,17 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "NetlandAPI");
+});
 
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-app.UseAuthorization();
 
 app.MapRazorPages();
 
